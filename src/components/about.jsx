182789a13a2f98a8983a3f9c6099a1454a33eb34
Amanuel-Naizghi <naizghiaman@gmail.com>
2025-04-25 08:17:08 +0300

    Dark mode design for about me page has been optimized

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,16 +2,20 @@ import Header from './header';
 import education from '../assets/education.png';
 import passion from '../assets/passion.png';
 import myPhoto from '../assets/my-photo.jpeg';
+import { ThemeContext } from './ThemeContext';
+import { useContext } from 'react';
 
 export default function About() {
+    const {theme}=useContext(ThemeContext);
+
     return (
-      <div className="about-me-container">
+      <div className={`${theme==='white'?"bg-white":"bg-black"}`}>
              <Header></Header>
-             <div className="p-8 mt-[4rem] flex flex-col items-center justify-center">
-                <p className="text-gray-600 text-center mb-4">
+             <div className="p-8 pt-5 mt-[4rem] flex flex-col items-center justify-center min-h-screen">
+                <p className={`text-center mb-4 ${theme==="white"?"text-gray-600":"text-white"}`}>
                     Welcome to my portfolio!
                 </p>        
-                <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">
+                <h1 className={`text-3xl font-bold text-center mb-8 ${theme==="white"?"text-gray-800":"text-white"}`}>
                     About Me
                 </h1>
                 <div className="flex flex-col lg:grid lg:grid-cols-3 gap-6 lg:h-[400px] lg:w-[800px] flex items-center">
@@ -27,14 +31,14 @@ export default function About() {
                             <div className="text-blue-500 text-4xl mb-2">
                                 <img className="h-[70px]" src={education} alt="Education" />
                             </div>
-                            <h2 className="text-[1.2rem] font-semibold text-gray-800 mb-2">
+                            <h2 className={`text-[1.2rem] font-bold text-center mb-8 ${theme==="white"?"text-gray-800":"text-white"}`}>
                                 Education
                             </h2>
-                            <p className="text-gray-600 text-[1rem] mb-2">
+                            <p className={`pl-[1rem] ${theme==="white"?"text-black":"text-white"}`}>
                                 B.Sc Bachelors Degree
                             </p>
-                            <p className="text-gray-600 text-[1rem]">
-                                Computer Science
+                            <p className={`pl-[1rem] ${theme==="white"?"text-black":"text-white"}`}>
+                                Computer Science 2014-2019
                             </p>
                         </div>
                     </div>
@@ -43,13 +47,13 @@ export default function About() {
                             <div className="text-green-500 text-4xl mb-2 flex justify-center">
                                 <img src={passion} alt="Passion" />
                             </div>
-                            <h2 className="text-[1.2rem] font-semibold text-gray-800 mb-2 flex justify-center">
+                            <h2 className={`text-[1.2rem] font-bold text-center mb-8 ${theme==="white"?"text-gray-800":"text-white"}`}>
                                 My Passion
                             </h2>
-                            <p className="text-gray-600 text-[1rem] mb-2">
+                            <p className={`pl-[1rem] ${theme==="white"?"text-black":"text-white"}`}>
                                 Building innovative projects that solve real-world problems
                             </p>
-                            <p className="text-gray-600 text-[1rem]">
+                            <p className={`pl-[1rem] ${theme==="white"?"text-black":"text-white"}`}>
                                 Always eager to learn and grow in the tech field.
                             </p>
                         </div>
@@ -57,10 +61,10 @@ export default function About() {
             
                     {/* Second row, second column (spans two columns) */}
                     <div className="col-span-2 lg:h-[120px] lg:w-[670px]">
-                        <p className="text-gray-600 text-[1.1rem] mb-[1rem]">
+                        <p className={`pl-[1rem] ${theme==="white"?"text-black":"text-white"}`}>
                         Outside of development, I enjoy exploring the outdoors, reading, and connecting with people from different walks of life. My goal is to create meaningful impacts through technology.
                         </p>
-                        <em className="text-gray-600 text-[1.1rem]">"Don’t count the days, make the days count." --- Muhammad Ali</em>
+                        <em className={`pl-[1rem] ${theme==="white"?"text-black":"text-white"}`}>"Don’t count the days, make the days count." --- Muhammad Ali</em>
                     </div>
                 </div>
             </div>
